79022049948e53bf8a801bf8b25f196229cc146d
CelesteHueso <celestehueso@gmail.com>
2022-05-16 23:04:04 +0200

    Menú animación responsive

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,12 +1,13 @@
 $(function (){ //$(document).ready(function () {});
 //Código jQuery
 	console.log('llega aqui');
-	$('#menuPrincipalIcono').click(function (){
+	$('#nav-icon3').click(function (){
 		// Sentencias que se ejecutan al hacer click sobre el ícono
-		$('#tucola ul').toggleClass('abierto');// como el toggleClass ya indica que es una clase no hace falta poner un punto
+		$('#menuP ul').toggleClass('abierto');// como el toggleClass ya indica que es una clase no hace falta poner un punto
+		$('#nav-icon3').toggleClass('open'); //esta función 
 	});
-	$('#tucola a').click(function(){ //con esta acción hacemos que se cierre el menú al cliclar en un <a>
-		$('#tucola ul').removeClass('abierto');
+	//$('#menuP a').click(function(){ //con esta acción hacemos que se cierre el menú al cliclar en un <a>
+	//	$('#menuP ul').removeClass('abierto');});
 
-	});
+	
 });
